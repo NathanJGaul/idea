b7bc0ccd995a7b77cc9bde45fc0bf5537d94528d
Nathan Gaul <nathanjgaul@gmail.com>
2026-03-02 16:03:35 -0600

    Invalidate and regenerate auth session token upon logout to ensure fresh token is used.

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -30,9 +30,12 @@ class SessionsController extends Controller
         return redirect()->intended('/')->with('success', 'You have successfully logged in!');
     }
 
-    public function destroy()
+    public function destroy(Request $request)
     {
         Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
 
         return redirect('/');
     }
